7de5283688c18dac925acb54b6785b46778200d6
Mohammed <m97.chahboun@gmail.com>
2023-11-25 22:08:43 +0100

    format codes

--- src/components/nav_bar.tsx
+++ src/components/nav_bar.tsx
@@ -1,8 +1,8 @@
 
 import logo from '../assets/logo.svg';
 
-function NavBar(){
-    const items = ['Home', 'Product', 'About', 'Contact'].map(item=><NavBarItem title={item} />);
+function NavBar() {
+    const items = ['Home', 'Product', 'About', 'Contact'].map(item => <NavBarItem title={item} />);
     return (
         <div className=' bg-neutral-800 flex flex-row justify-evenly items-center h-[119px]'>
             <div>
@@ -10,7 +10,7 @@ function NavBar(){
             </div>
             <nav>
                 <ul className="flex flex-row gap-[31px]">
-                {items}
+                    {items}
                 </ul>
             </nav>
             <a className='py-3 px-10 text-slate-50 border-white border-2' href='blank'>Login</a>
@@ -22,7 +22,7 @@ type NavBarItemType = {
     title: string
 }
 
-function NavBarItem({title}:NavBarItemType){
+function NavBarItem({ title }: NavBarItemType) {
     return <li><a className=' text-slate-50' href="#{title}">{title}</a></li>;
 }
 
